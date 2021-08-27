bcbe67e47b6e0e5db47540073d4cf68864cabe89
chrisliatas <chris@liatas.com>
2021-08-27 22:08:10 +0000

    update run.py, rewrite template

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Histogram
 import joblib
 from sqlalchemy import create_engine
 
@@ -44,10 +44,39 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby("genre").count()["message"]
     genre_names = list(genre_counts.index)
+    categories_perc = df.iloc[:, 4:].sum()
+    category_names = [c.replace("_", " ") for c in df.iloc[:, 4:].columns]
+    df["message_length"] = df["message"].str.len()
 
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        {
+            "data": [Bar(x=category_names, y=categories_perc)],
+            "layout": {
+                "title": "Messages categories",
+                "yaxis": {"title": "Count"},
+                "xaxis": {
+                    "title": "Categories",
+                    "tickangle": 25,
+                    "title_standoff": 100,
+                },
+            },
+        },
+        {
+            "data": [
+                Histogram(x=df["message_length"], y=df["message_length"].value_counts())
+            ],
+            "layout": {
+                "title": "Message length frequency",
+                "yaxis": {"title": "Frequency"},
+                "xaxis": {
+                    "title": "Message length (chars)",
+                    # "tickangle": 45,
+                    "title_standoff": 100,
+                },
+            },
+        },
         {
             "data": [Bar(x=genre_names, y=genre_counts)],
             "layout": {
@@ -55,7 +84,7 @@ def index():
                 "yaxis": {"title": "Count"},
                 "xaxis": {"title": "Genre"},
             },
-        }
+        },
     ]
 
     # encode plotly graphs in JSON
@@ -87,4 +116,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
